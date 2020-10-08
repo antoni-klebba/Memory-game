fefc12c6bd4da3f6004c231f62cd576bf6f609e2
Antoni Klebba <antoni.klebba@gmail.com>
2020-10-08 08:27:47 +0200

    Add simple player score system

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ const panel = document.querySelector('.panel')
 let cards = document.querySelectorAll("main div");
 cards = [...cards];
 
+// Variables for Cards
 let activeCard = "";
 const activeCards = [];
 const gamePairs = cards.length / 2;
 let gameResult = 0;
+//Variable for delay
 let delay = 2000;
+// Variables for playerScore
+let startTime = 0
+let endTime = 0
+let correctAnswers = 0;
+let incorrectAnswers = 0;
 
 const loadPage = () => cards.forEach(card => card.classList.add('hidden'))
 
@@ -28,6 +35,7 @@ const startGame = () => {
             cards.forEach(card => {
                 card.classList.add('hidden');
                 card.addEventListener('click', clickCard);
+                startTime = new Date().getTime();
             })
         }, delay)
     }
@@ -35,11 +43,8 @@ const startGame = () => {
 
 const clickCard = function () {
     activeCard = this;
-
     if (activeCard == activeCards[0]) return;
-
     activeCard.classList.remove("hidden");
-
     if (activeCards.length === 0) {
         activeCards[0] = activeCard
         return;
@@ -51,44 +56,56 @@ const clickCard = function () {
                 activeCards.forEach(activeCard => activeCard.classList.add('off-transition'));
                 activeCards.forEach(activeCard => activeCard.classList.add('off'));
                 gameResult++;
+                correctAnswers++;
                 cards = cards.filter(card => !card.classList.contains('off'));
                 if (gameResult == gamePairs) {
-                    clearInterval(idI)
+                    clearInterval(idI);
+                    endTime = new Date().getTime();
+                    playerScore();
                 }
             } else {
                 activeCards.forEach(activeCard => activeCard.classList.add('hidden'))
+                incorrectAnswers++;
             }
             activeCard = "";
             activeCards.length = 0;
             cards.forEach(card => card.addEventListener('click', clickCard))
-        }, 500)
+        }, 450)
     }
 }
 
 const buttonTextContent = () => {
-    setTimeout(function () {
-        startReset.textContent = "Reset";
-        startReset.classList.add('reset');
-    }, delay)
+    startReset.textContent = "Reset";
+    startReset.classList.add('reset');
 }
 
-// Timer
-let time = 0; //dziesiętne sekundy
+// stopwatch
+const spanM = document.querySelector('span.minutes');
+const spanS = document.querySelector('span.seconds');
+const spanC = document.querySelector('span.centiseconds');
+
 let idI;
 
-const timer = () => {
-    if (startReset.classList.contains("reset")) document.location.reload(true);
-    else {
-        setTimeout(function () {
-            idI = setInterval(start, 100);
-        }, delay)
-    }
-    const start = () => {
-        time++;
-        panel.textContent = `${(time / 10).toFixed(1)} s`;
-    }
+const stopwatch = () => {
+    setTimeout(function () {
+        idI = setInterval(() => {
+            const nowTime = new Date().getTime();
+            const time = nowTime - startTime
+
+            let minutes = Math.floor(time / (1000 * 60));
+            minutes = minutes < 10 ? `0${minutes}` : minutes;
+            spanM.textContent = minutes;
+
+            let seconds = Math.floor(time / 1000 % 60);
+            seconds = seconds < 10 ? `0${seconds}` : seconds;
+            spanS.textContent = seconds;
+
+            let centiseconds = Math.floor(time / 10 % 100);
+            centiseconds = centiseconds < 10 ? `0${centiseconds}` : centiseconds;
+            spanC.textContent = centiseconds;
+        }, 10)
+    }, delay)
 }
-//End
 
 // Delay
 const input = document.querySelector('.delay input');
@@ -98,17 +115,43 @@ function handleUpdate() {
     input.value = this.value;
     delay = input.value * 1000;
     delayTime.textContent = `${delay / 1000}s`
+    startingScore = 1000 / (delay / 1500);
 }
 
 input.addEventListener('change', handleUpdate)
 input.addEventListener('mousemove', handleUpdate)
-//End
 
+//Player score
+const playerScore = () => {
+    let startingScore = 1000 / (delay / 1750);
+
+    if (startingScore === Infinity) startingScore = 1500;
+    else if (delay >= 5000) startingScore = 1000 / (delay / 3000);
+    else if (delay >= 8000) startingScore = 1000 / (delay / 3500);
+    const amountOfSeconds = (endTime - startTime) / 1000;
+    const correctAnswersMultiplier = Math.sqrt(correctAnswers / incorrectAnswers)
+
+    const endingScore = ((startingScore / amountOfSeconds) * correctAnswersMultiplier).toFixed(2);
+
+    if (endingScore > 100) {
+        alert(`Idziesz na rekord!!! Twój wynik to: ${endingScore}!`);
+    } else if (endingScore > 75) {
+        alert(`Świetnie Ci poszło! Twój wynik to: ${endingScore}!`);
+    } else if (endingScore < 15) {
+        alert(`Nie poszło najlepiej... Twój wynik to ${endingScore}`)
+    } else alert(`Twój wynik z tej gry to: ${endingScore}!`);
+
+    console.log(startingScore);
+    console.log(amountOfSeconds);
+    console.log(correctAnswers);
+    console.log(incorrectAnswers);
+    console.log(correctAnswersMultiplier);
+    console.log(endingScore);
+}
+
+//End
 startReset.addEventListener('click', startGame);
-startReset.addEventListener('click', timer);
+startReset.addEventListener('click', stopwatch);
 startReset.addEventListener('click', buttonTextContent);
 
-
-// init();
-// timer();
 loadPage();
